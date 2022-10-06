d0401946c2803391853694625209224dbf13947f
vobish <urijgula@gmail.com>
2022-10-06 11:53:34 +0300

    commit

--- cypress/support/customCommands.d.ts
+++ cypress/support/customCommands.d.ts
@@ -4,21 +4,16 @@ declare namespace Cypress {
       loginForm(username: any, password: any): Chainable<any>
       selectCustomer(searchClient: any, clientName: any): Chainable<any>
       setUpInvoice(element: any): Chainable<any>
-      goToPage(root: any): Chainable<any>
       comparePrice(element: any): Chainable<any>
       deleteDraftInvoice(element: any): Chainable<any>
 
       //------Commands
 
-        comparePrice(element: any): Chainable<any>
-        deleteDraftInvoice(element: any): Chainable<any>
-
-        iFrameCommand(element: any): Chainable<any>
-        duplicateWindow(element: any): Chainable<any>
-        
-        elementIsVisible(element: any): Chainable<any>
-        elementloaded(element: any): Chainable<any>
-        elementExist(element: any): Chainable<any>
+      goToPage(root: any): Chainable<any>
+      duplicateWindow(element: any): Chainable<any>        
+      elementIsVisible(element: any): Chainable<any>
+      elementloaded(element: any): Chainable<any>
+      elementExist(element: any): Chainable<any>
         
   }
 }
